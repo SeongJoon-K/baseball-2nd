b3020b53879adab1c11d854054c5b055aa237aef
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-27 15:23:21 +0900

    feat: 사용자의 숫자 입력 메세지를 출력하는 메소드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,11 @@
 package baseball.view;
 
 public class OutputView {
-    public void printGameStartMsg() {
+    public static void printGameStartMsg() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printUserNumberInputMsg() {
+        System.out.println("숫자를 입력해주세요 : ");
     }
 }
